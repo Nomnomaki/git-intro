0ec8631dc9d5b7f333bdcedc526f4d08fc7216e4
Aniket <aniket27.saxena@gmail.com>
2023-12-26 22:52:04 +0530

    version 2

--- bye.c
+++ bye.c
@@ -2,5 +2,6 @@
 int main() {
    
    printf("Bye, World!");
+   printf("Bye, World!");
    return 0;
-}
+}   
--- hi.c
+++ hi.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main() {
-   printf("Hello, World!");
+   printf("Hello1, World!");
    return 0;
 }
